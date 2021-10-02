065f51cad253a756870636fd01e1bc38b0669dae
Tanha Reja <tanurezza99@gmail.com>
2021-10-02 19:29:09 +0600

    CV

---
+++ CV/CV/Controllers/ProfileController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Bio()
+        {
+            ViewData["Name"] = "Tanha Reja";
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
